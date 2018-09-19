6310ed3b76e1b0c2436f0e5cdb63ad17caaf036a
moritzkitzmann <mkitzmann@gmail.com>
2018-09-19 11:00:05 +0200

    Improve Preload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,21 @@
 
-
 var classname = document.getElementsByClassName("gallery-next");
 var classname2 = document.getElementsByClassName("gallery-prev");
 
 
 var callback = function() {
-    var fadeImage = document.getElementById("fadeout");
     event.preventDefault();
+    var fadeImage = document.getElementById("fadeout");
+
+    var spinner = document.getElementsByClassName('spinner');
+
+    for (var i = 0; i < spinner.length; i++) {
+        spinner[i].style.visibility = "hidden";
+    }
+
     fadeImage.classList.add('fadeout');
     fadeImage.classList.remove('fadein');
+
     var newLocation = this.href;
     window.location.assign(newLocation);    
 };
